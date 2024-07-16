909806d684d2c7462bc4121bf56dd4b6c9928329
zi9987 <peterluoluo25@gmail.com>
2024-07-16 14:11:58 +0800

    Initial Commit

---
+++ SwiftUiMapApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUiMapApp
+//
+//  Created by 羅子淵 on 2024/7/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUiMapApp/SwiftUiMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiMapAppApp.swift
+//  SwiftUiMapApp
+//
+//  Created by 羅子淵 on 2024/7/16.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
